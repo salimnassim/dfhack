abe2ffa3b3c04d7e3b6b781f17a9abb7920ff9d1
Salim Nassim <salim.nassim@gmail.com>
2026-09-24 11:42:27 +0200

    add: context add: docs change: fmt print text callback

--- cmd/command.go
+++ cmd/command.go
@@ -1,3 +1,9 @@
+// Command cmd runs a single DFHack console command over RPC and prints its
+// output.
+//
+// Usage:
+//
+//	cmd [-addr host:port] [-command name] [args...]
 package main
 
 import (
@@ -5,25 +11,30 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/salimnassim/dfhack/client"
 	pb "github.com/salimnassim/dfhack/gen/proto"
 )
 
 func main() {
-	address := flag.String("addr", client.DefaultAddr, "remote server address")
+	addr := flag.String("addr", client.DefaultAddr, "remote server address")
 	command := flag.String("command", "ls", "console command to run")
-	args := flag.Args()
-
 	flag.Parse()
 
-	err := run(context.Background(), *address, command, args)
-	if err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx, *addr, *command, flag.Args()); err != nil {
 		slog.Error("failed to run dfhack command", "error", err)
+		stop()
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, addr string, command *string, args []string) error {
+func run(ctx context.Context, addr, command string, args []string) error {
 	c, err := client.Dial(ctx, addr)
 	if err != nil {
 		return err
@@ -32,14 +43,10 @@ func run(ctx context.Context, addr string, command *string, args []string) error
 
 	c.OnText = func(n *pb.CoreTextNotification) {
 		for _, fragment := range n.GetFragments() {
-			slog.Info(fmt.Sprintf("%s", fragment.GetText()))
+			fmt.Print(fragment.GetText())
 		}
 	}
 
-	cmd := &pb.CoreRunCommandRequest{Command: command, Arguments: args}
-	if err := c.Call(client.RunCommandID, cmd, &pb.EmptyMessage{}); err != nil {
-		return err
-	}
-
-	return nil
+	req := &pb.CoreRunCommandRequest{Command: new(command), Arguments: args}
+	return c.Call(ctx, client.RunCommandID, req, &pb.EmptyMessage{})
 }
